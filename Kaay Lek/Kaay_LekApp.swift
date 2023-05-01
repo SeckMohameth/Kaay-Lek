cf9f285c69388cf60bb9a414b1d89bfe18107827
SeckMohameth <mohamethseck34@gmail.com>
2023-04-30 21:50:09 -0400

    Initial Commit

---
+++ Kaay Lek/Kaay_LekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kaay_LekApp.swift
+//  Kaay Lek
+//
+//  Created by Mohameth Seck on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Kaay_LekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
